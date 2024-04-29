d30bd63a94348e4d48cde55fb3291895540674f8
Joel <jrevertvila@gmail.com>
2024-04-30 00:08:18 +0200

    mejorar carrusel

--- src/components/Modelos3D_carousel_2/modelos3d_carousel.jsx
+++ src/components/Modelos3D_carousel_2/modelos3d_carousel.jsx
@@ -2,12 +2,12 @@
 import './modelos3d_carousel.css'
 import LogoArtemisPNG from '../../assets/ModelosPNG/LogoArtemisModelo3d.png'
 import Modelo3d_item from './Modelo3d_item';
+import { useEffect, useRef, useState } from 'react';
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { FaRegArrowAltCircleLeft } from "react-icons/fa";
+
 
 export default function Modelos3d_2() {
-    const listOfCardElements = document.querySelectorAll('.card_mod');
-    const cardContainer = document.querySelector('.card_mod-container');
-    const carouselContainer = document.querySelector('.carousel');
-
     const modelos3d_json = {
         modelo1: {
             title: "Falla Principal",
@@ -96,97 +96,172 @@ export default function Modelos3d_2() {
             src: LogoArtemisPNG,
             modelo3d_filename: "Scene",
             author: "Maria",
-        }
+        },
+        modelo12: {
+            title: "Falla Principal",
+            subtitle: "Este modelo es el monumento principal de la falla",
+            alt: "Modelo3d",
+            src: LogoArtemisPNG,
+            modelo3d_filename: "Scene",
+            author: "Maria",
+        },
+        modelo13: {
+            title: "Falla Principal",
+            subtitle: "Este modelo es el monumento principal de la falla",
+            alt: "Modelo3d",
+            src: LogoArtemisPNG,
+            modelo3d_filename: "Scene",
+            author: "Maria",
+        },
+        modelo14: {
+            title: "Falla Principal",
+            subtitle: "Este modelo es el monumento principal de la falla",
+            alt: "Modelo3d",
+            src: LogoArtemisPNG,
+            modelo3d_filename: "Scene",
+            author: "Maria",
+        },
+        modelo15: {
+            title: "Falla Principal",
+            subtitle: "Este modelo es el monumento principal de la falla",
+            alt: "Modelo3d",
+            src: LogoArtemisPNG,
+            modelo3d_filename: "Scene",
+            author: "Maria",
+        },
+        modelo16: {
+            title: "Falla Principal",
+            subtitle: "Este modelo es el monumento principal de la falla",
+            alt: "Modelo3d",
+            src: LogoArtemisPNG,
+            modelo3d_filename: "Scene",
+            author: "Maria",
+        },
+        modelo17: {
+            title: "Falla Principal",
+            subtitle: "Este modelo es el monumento principal de la falla",
+            alt: "Modelo3d",
+            src: LogoArtemisPNG,
+            modelo3d_filename: "Scene",
+            author: "Maria",
+        },
+        modelo18: {
+            title: "Falla Principal",
+            subtitle: "Este modelo es el monumento principal de la falla",
+            alt: "Modelo3d",
+            src: LogoArtemisPNG,
+            modelo3d_filename: "Scene",
+            author: "Maria",
+        },
     }
 
-    let keys = Object.keys(modelos3d_json).length
+    let keys = Object.keys(modelos3d_json)
     let pages = Math.ceil(Object.keys(modelos3d_json).length / 6)
-
     let cont_object = 0
+
+    const [active, setActive] = useState(false)
+    const [down, setDown] = useState(false)
+    const [startX, setStartX] = useState(false)
+    const [scrollLeft, setScrollLeft] = useState(false)
+    const [move_, setMove_] = useState(0)
+    const [maxScroll, setMaxScroll] = useState(0)
+    const [maxScrollPaged, setMaxScrollPaged] = useState(0)
+    const carouselRef = useRef(null);
+
+
+    useEffect(() => {
+        carouselRef.current.scrollLeft = move_;
+    }, [move_]);
+
+    useEffect(() => {
+        carouselRef.current.scrollLeft = move_;
+        setMaxScroll(Math.ceil(carouselRef.current.scrollWidth - carouselRef.current.clientWidth))
+        setMaxScrollPaged(Math.ceil(maxScroll / pages))
+        console.log("carouselRef.current.scrollWidth", carouselRef.current.scrollWidth);
+        console.log("carouselRef.current.clientWidth", carouselRef.current.clientWidth);
+        console.log(maxScroll);
+        console.log(maxScrollPaged);
+        console.log(move_);
+   
+    }, []);
+
+    let mousedown = (e) => {
+        setDown(true)
+        setActive(true)
+        setStartX(e.pageX - carouselRef.current.offsetLeft)
+        setScrollLeft(carouselRef.current.scrollLeft)
+    }
+
+    // useEffect(() => {
+    //     carouselRef.current.scrollLeft = move_;
+    // }, [move_]);
+
+    let mousemove = (e) => {
+        if (!down) return;
+        e.preventDefault();
+        const x = e.pageX - carouselRef.current.offsetLeft;
+        const walk = (x - startX) * 3;
+        setMove_(scrollLeft + walk)
+        carouselRef.current.scrollLeft = move_;
+    }
+
+    let mouseleave = () => {
+        setDown(false);
+        setActive(false)
+    }
+
+    let mouseup = () => {
+        setDown(false);
+        setActive(false)
+    }
+
+    let setMoveFn = (m) => {
+        console.log("move_", move_);
+        console.log("maxScrollPaged", maxScrollPaged);
+        console.log("m antes",m);
+        if (m < 0) m = 0
+        if (m > maxScroll) m = maxScroll
+        console.log("m despues",m);
+        setMove_(m)
+    }
 
     return (
         <div className='app__wrapper modelos_page_container'>
             <h2 className='modelos__page_title'>Modelos 3D</h2>
             <div className="carousel">
-                <div className="card_mod-container">
+                <div className='anterior' onClick={() => setMoveFn(move_ - (maxScrollPaged))}>
+                    <FaRegArrowAltCircleLeft />
+                </div>
+                <div id='card_mod-container' ref={carouselRef} className={"card_mod-container" + (active ? ' active_drag' : '')} onMouseDown={(e) => mousedown(e)} onMouseLeave={() => mouseleave()} onMouseMove={(e) => mousemove(e)} onMouseUp={() => mouseup()}>
                     {(() => {
                         let content = []
-
                         for (let j = 0; j < pages; j++) {
-                            console.log("pagina " + j );
                             content.push(
-                                <div className="card_mod" key={"key_card"+j}>
+                                <div className="card_mod" key={"key_card" + j}>
                                     {(() => {
                                         let content_items = []
                                         let contador = 0
-                                        for (let x = cont_object; x < keys; x++) {
+                                        for (let x = cont_object; x < keys.length; x++) {
 
                                             if (contador == 6) break;
                                             contador++
-                                            const modelo_item = modelos3d_json[x];
-                                            console.log(modelo_item);
-                                            console.log("dentro for");
+                                            const modelo_item = modelos3d_json[keys[x]];
                                             content_items.push(
-                                                <Modelo3d_item key={"item"+cont_object} alt={modelo_item['alt']} src={modelo_item['src']} title={modelo_item['title'] + x} subtitle={modelo_item['subtitle']} modelo3d_filename={modelo_item['modelo3d_filename']} author={modelo_item['author']} />
-
-
+                                                <Modelo3d_item key={"item" + cont_object} alt={modelo_item['alt']} src={modelo_item['src']} title={modelo_item['title'] + x} subtitle={modelo_item['subtitle']} modelo3d_filename={modelo_item['modelo3d_filename']} author={modelo_item['author']} />
                                             )
                                             cont_object++
                                         }
-                                        console.log(content_items);
                                         return content_items
-
                                     })()}
-
-
-
                                 </div>
                             )
-
                         }
                         return content
-
                     })()}
-                    {/* <div className="card_mod">
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla Principal"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
 
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 2"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 3"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 4"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla Principal"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 2"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-                    </div>
-                    <div className="card_mod">
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla Principal"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 2"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 3"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 4"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla Principal"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 2"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-                    </div>
-                    <div className="card_mod">
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla Principal"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 2"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 3"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 4"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla Principal"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-
-                        <Modelo3d_item alt={"Modelo3d"} src={LogoArtemisPNG} title={"Falla 2"} subtitle={"Este modelo es el monumento principal de la falla"} modelo3d_filename={"Scene"} author={"Maria"} />
-                    </div> */}
-
+                </div>
+                <div className='siguiente' onClick={() => setMoveFn(move_ + maxScrollPaged)}>
+                    <FaRegArrowAltCircleRight />
                 </div>
             </div>
 
