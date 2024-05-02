269ce72d135c4a17a1d3d5601a2633e6ea6d6935
Celia <celiagarciamonforte@gmail.com>
2024-05-02 12:13:30 +0200

    accesibility section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import { Home } from './components/Home/Home.jsx'
 import { AboutUs } from './components/AboutUs/AboutUs.jsx'
 // import Modelos3D from './components/Modelos3D/Modelos.jsx'
-import ViveExperiencia from './components/ViveLaExperiencia/ViveExperiencia.jsx'
 // import Modelos3D_Carrusel from './components/Modelos3D_carrusel/Modelos_Carrusel.jsx'
 import Modelos3d_2 from './components/Modelos3D_carousel_2/modelos3d_carousel.jsx'
 import { AboutProject } from './components/AboutProject/AboutProject.jsx'
@@ -11,6 +10,7 @@ import { useTranslation } from 'react-i18next'
 import { useEffect, useState } from 'react'
 import { Header } from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import Accesibilidad from './components/Accesibilidad/Accesibilidad.jsx'
 
 function App() {
   const { t, i18n } = useTranslation()
@@ -28,7 +28,7 @@ function App() {
       <AboutProject />
 
       <Modelos3d_2/>
-      {/* <ViveExperiencia /> */}
+      <Accesibilidad />
       <AboutUs />
       <Footer />
     </>
