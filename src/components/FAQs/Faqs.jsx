5deb71052f9cf9a1f1419dba7d2a24df8b0d0716
Joel Revert <jrevertvila@gmail.com>
2024-05-14 09:36:25 +0200

    update faqs

--- src/components/FAQs/Faqs.jsx
+++ src/components/FAQs/Faqs.jsx
@@ -10,32 +10,42 @@ export default function Faqs() {
             <p className='faqs_subtitle'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
             <div className='faqs_wrapper'>
                 <FaqsItem
-                    pregunta={"¿El juego está ya terminado?"}
+                    pregunta={"¿Cómo surge la idea de crear una experiencia de realidad virtual sobre las Fallas?"}
                     respuesta={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."}
                 />
 
                 <FaqsItem
-                    pregunta={"¿La finalidad del juego es sustituir a Las Fallas?"}
+                    pregunta={"¿Cuál es el objetivo de este proyecto de realidad virtual?"}
                     respuesta={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."}
                 />
 
                 <FaqsItem
-                    pregunta={"¿El juego está ya terminado?"}
+                    pregunta={"¿Quiénes pueden disfrutar de esta experiencia de realidad virtual?"}
                     respuesta={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."}
                 />
 
                 <FaqsItem
-                    pregunta={"¿El juego está ya terminado?"}
+                    pregunta={"¿Cuál es el alcance futuro de este proyecto?"}
                     respuesta={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."}
                 />
 
                 <FaqsItem
-                    pregunta={"¿El juego está ya terminado?"}
+                    pregunta={"¿Se planea ofrecer esta experiencia en otros idiomas además del valenciano?"}
                     respuesta={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."}
                 />
 
                 <FaqsItem
-                    pregunta={"¿El juego está ya terminado?"}
+                    pregunta={"¿Qué medidas de accesibilidad se han implementado para garantizar que esta experiencia sea inclusiva para todos los usuarios?"}
+                    respuesta={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."}
+                />
+
+                <FaqsItem
+                    pregunta={"¿El juego ya está terminado?"}
+                    respuesta={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."}
+                />
+
+                <FaqsItem
+                    pregunta={"¿Dónde puedo acceder a esta experiencia de realidad virtual?"}
                     respuesta={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."}
                 />
             </div>
